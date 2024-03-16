8f8c2325ba8185813487082ed0f20d7f2abc3391
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-16 10:20:22 -0300

    feat: add CreateCategoryUseCase implementation

--- application/src/test/java/com/fullcycle/admin/catalogo/application/UseCaseTest.java
+++ application/src/test/java/com/fullcycle/admin/catalogo/application/UseCaseTest.java
@@ -1,11 +1,8 @@
 package com.fullcycle.admin.catalogo.application;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 public class UseCaseTest {
