a35ebdbb5c30e515ef89b836af9afa36b6629514
Flávio Dutra <flaviomdutra@outlook.com>
2024-03-31 14:23:00 -0300

    feat: integration tests

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -14,9 +14,9 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Component;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.StreamSupport;
 
 import static com.fullcycle.admin.catalogo.infrastructure.utils.SpecificationUtils.like;
 
@@ -90,7 +90,13 @@ public class CategoryMySQLGateway implements CategoryGateway {
     }
 
     @Override
-    public List<CategoryID> existsByIds(final Iterable<CategoryID> ids) {
-        return Collections.emptyList();
+    public List<CategoryID> existsByIds(final Iterable<CategoryID> categoryIDS) {
+        final var ids = StreamSupport.stream(categoryIDS.spliterator(), false)
+                .map(CategoryID::getValue)
+                .toList();
+
+        return this.repository.existsByIds(ids).stream()
+                .map(CategoryID::from)
+                .toList();
     }
 }
