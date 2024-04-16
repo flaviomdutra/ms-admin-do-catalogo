3ac8b3889dbd603083ce82e4e9cdf9e47e4faede
Flávio Dutra <flaviomdutra@outlook.com>
2024-04-16 11:04:12 -0300

    feat: implement CastMemberController

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CastMemberController.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CastMemberController.java
@@ -1,5 +1,7 @@
 package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
 
+import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberCommand;
+import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberUseCase;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admin.catalogo.infrastructure.api.CastMemberAPI;
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CastMemberListResponse;
@@ -7,11 +9,28 @@ import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CastMemberR
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CreateCastMemberRequest;
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.UpdateCastMemberRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.net.URI;
+import java.util.Objects;
+
+@RestController
 public class CastMemberController implements CastMemberAPI {
+
+    private final CreateCastMemberUseCase createCastMemberUseCase;
+
+    public CastMemberController(final CreateCastMemberUseCase createCastMemberUseCase) {
+        this.createCastMemberUseCase = Objects.requireNonNull(createCastMemberUseCase);
+    }
+
     @Override
-    public ResponseEntity<?> create(CreateCastMemberRequest input) {
-        return null;
+    public ResponseEntity<?> create(final CreateCastMemberRequest input) {
+        final var aCommand =
+                CreateCastMemberCommand.with(input.name(), input.type());
+
+        final var output = this.createCastMemberUseCase.execute(aCommand);
+
+        return ResponseEntity.created(URI.create("/cast_members/" + output.id())).body(output);
     }
 
     @Override
