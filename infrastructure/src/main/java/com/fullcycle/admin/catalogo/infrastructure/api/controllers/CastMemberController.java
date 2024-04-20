d64d1693b80b30020766a7d7a169039690e4652a
Flávio Dutra <flaviomdutra@outlook.com>
2024-04-20 14:22:55 -0300

    feat: implement apis from CastMember

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CastMemberController.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/api/controllers/CastMemberController.java
@@ -2,8 +2,13 @@ package com.fullcycle.admin.catalogo.infrastructure.api.controllers;
 
 import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberCommand;
 import com.fullcycle.admin.catalogo.application.castmember.create.CreateCastMemberUseCase;
+import com.fullcycle.admin.catalogo.application.castmember.delete.DeleteCastMemberUseCase;
 import com.fullcycle.admin.catalogo.application.castmember.retrieve.get.GetCastMemberByIdUseCase;
+import com.fullcycle.admin.catalogo.application.castmember.retrieve.list.ListCastMembersUseCase;
+import com.fullcycle.admin.catalogo.application.castmember.update.UpdateCastMemberCommand;
+import com.fullcycle.admin.catalogo.application.castmember.update.UpdateCastMemberUseCase;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
+import com.fullcycle.admin.catalogo.domain.pagination.SearchQuery;
 import com.fullcycle.admin.catalogo.infrastructure.api.CastMemberAPI;
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CastMemberListResponse;
 import com.fullcycle.admin.catalogo.infrastructure.castmember.models.CastMemberResponse;
@@ -21,10 +26,22 @@ public class CastMemberController implements CastMemberAPI {
 
     private final CreateCastMemberUseCase createCastMemberUseCase;
     private final GetCastMemberByIdUseCase getCastMemberByIdUseCase;
+    private final UpdateCastMemberUseCase updateCastMemberUseCase;
+    private final DeleteCastMemberUseCase deleteCastMemberUseCase;
+    private final ListCastMembersUseCase listCastMembersUseCase;
 
-    public CastMemberController(final CreateCastMemberUseCase createCastMemberUseCase, final GetCastMemberByIdUseCase getCastMemberByIdUseCase) {
+    public CastMemberController(
+            final CreateCastMemberUseCase createCastMemberUseCase,
+            final GetCastMemberByIdUseCase getCastMemberByIdUseCase,
+            final UpdateCastMemberUseCase updateCastMemberUseCase,
+            final DeleteCastMemberUseCase deleteCastMemberUseCase,
+            final ListCastMembersUseCase listCastMembersUseCase
+    ) {
         this.createCastMemberUseCase = Objects.requireNonNull(createCastMemberUseCase);
         this.getCastMemberByIdUseCase = Objects.requireNonNull(getCastMemberByIdUseCase);
+        this.updateCastMemberUseCase = Objects.requireNonNull(updateCastMemberUseCase);
+        this.deleteCastMemberUseCase = Objects.requireNonNull(deleteCastMemberUseCase);
+        this.listCastMembersUseCase = Objects.requireNonNull(listCastMembersUseCase);
     }
 
     @Override
@@ -38,8 +55,14 @@ public class CastMemberController implements CastMemberAPI {
     }
 
     @Override
-    public Pagination<CastMemberListResponse> list(String search, int page, int perPage, String sort, String direction) {
-        return null;
+    public Pagination<CastMemberListResponse> list(
+            final String search,
+            final int page,
+            final int perPage,
+            final String sort,
+            final String direction) {
+        return this.listCastMembersUseCase.execute(new SearchQuery(page, perPage, search, sort, direction))
+                .map(CastMemberPresenter::present);
     }
 
     @Override
@@ -48,12 +71,16 @@ public class CastMemberController implements CastMemberAPI {
     }
 
     @Override
-    public ResponseEntity<?> updateById(String id, UpdateCastMemberRequest aBody) {
-        return null;
+    public ResponseEntity<?> updateById(final String id, final UpdateCastMemberRequest aBody) {
+        final var aCommand = UpdateCastMemberCommand.with(id, aBody.name(), aBody.type());
+
+        final var output = this.updateCastMemberUseCase.execute(aCommand);
+
+        return ResponseEntity.ok(output);
     }
 
     @Override
-    public void deleteById(String id) {
-
+    public void deleteById(final String id) {
+        this.deleteCastMemberUseCase.execute(id);
     }
 }
