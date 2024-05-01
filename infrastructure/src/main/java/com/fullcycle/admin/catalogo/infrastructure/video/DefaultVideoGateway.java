f0e707646e27f2cd196eabdf55d16b7f52bbd42e
Flávio Dutra <flaviomdutra@outlook.com>
2024-05-01 15:55:20 -0300

    Add EventService interface and its implementations

--- infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultVideoGateway.java
+++ infrastructure/src/main/java/com/fullcycle/admin/catalogo/infrastructure/video/DefaultVideoGateway.java
@@ -3,6 +3,8 @@ package com.fullcycle.admin.catalogo.infrastructure.video;
 import com.fullcycle.admin.catalogo.domain.Identifier;
 import com.fullcycle.admin.catalogo.domain.pagination.Pagination;
 import com.fullcycle.admin.catalogo.domain.video.*;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.annotations.VideoCreatedQueue;
+import com.fullcycle.admin.catalogo.infrastructure.services.EventService;
 import com.fullcycle.admin.catalogo.infrastructure.utils.SqlUtils;
 import com.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoJpaEntity;
 import com.fullcycle.admin.catalogo.infrastructure.video.persistence.VideoRepository;
@@ -20,11 +22,14 @@ import static com.fullcycle.admin.catalogo.domain.utils.CollectionUtils.nullIfEm
 @Component
 public class DefaultVideoGateway implements VideoGateway {
 
+    private final EventService eventService;
     private final VideoRepository videoRepository;
 
     public DefaultVideoGateway(
+            @VideoCreatedQueue final EventService eventService,
             final VideoRepository videoRepository
     ) {
+        this.eventService = Objects.requireNonNull(eventService);
         this.videoRepository = Objects.requireNonNull(videoRepository);
     }
 
@@ -37,11 +42,9 @@ public class DefaultVideoGateway implements VideoGateway {
     @Override
     public void deleteById(final VideoID anId) {
         final var aVideoId = anId.getValue();
-
         if (this.videoRepository.existsById(aVideoId)) {
             this.videoRepository.deleteById(aVideoId);
         }
-
     }
 
     @Override
@@ -81,10 +84,11 @@ public class DefaultVideoGateway implements VideoGateway {
         );
     }
 
-
     private Video save(final Video aVideo) {
         final var result = this.videoRepository.save(VideoJpaEntity.from(aVideo))
                 .toAggregate();
+
+        aVideo.publishDomainEvents(this.eventService::send);
 
         return result;
     }
